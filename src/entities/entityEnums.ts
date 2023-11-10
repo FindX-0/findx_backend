756313bb3ed2550eb8dcfcf1ff61c4d8117deb77
kume1a <kumela011@gmail.com>
2023-11-10 22:24:15 +0400

    create matchmaking entities

--- src/entities/entityEnums.ts
+++ src/entities/entityEnums.ts
@@ -11,3 +11,16 @@ export const AuthProvider = {
   FACEBOOK: 'FACEBOOK',
 } as const;
 export type AuthProvider = (typeof AuthProvider)[keyof typeof AuthProvider];
+export const TicketState = {
+  PROCESSING: 'PROCESSING',
+  COMPLETED: 'COMPLETED',
+  CANCELLED: 'CANCELLED',
+  EXPIRED: 'EXPIRED',
+} as const;
+export type TicketState = (typeof TicketState)[keyof typeof TicketState];
+export const MatchState = {
+  PENDING: 'PENDING',
+  IN_PROGRESS: 'IN_PROGRESS',
+  FINISHED: 'FINISHED',
+} as const;
+export type MatchState = (typeof MatchState)[keyof typeof MatchState];
--- src/entities/entityTypes.ts
+++ src/entities/entityTypes.ts
@@ -4,33 +4,58 @@ export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
-import type { Gender, AuthProvider } from './entityEnums';
+import type {
+  Gender,
+  AuthProvider,
+  TicketState,
+  MatchState,
+} from './entityEnums';
 
 export type AccountVerification = {
   id: Generated<string>;
+  createdAt: Generated<Timestamp>;
   isVerified: Generated<boolean>;
   oneTimeCode: number;
   userId: string;
+};
+export type Match = {
+  id: Generated<string>;
+  createdAt: Generated<Timestamp>;
+  state: MatchState;
+};
+export type MathConcept = {
+  id: Generated<string>;
   createdAt: Generated<Timestamp>;
 };
 export type RefreshToken = {
   id: Generated<string>;
+  createdAt: Generated<Timestamp>;
   userId: string;
   value: string;
+};
+export type Ticket = {
+  id: Generated<string>;
   createdAt: Generated<Timestamp>;
+  mathConceptId: string;
+  userId: string;
+  state: TicketState;
+  matchId: string | null;
 };
 export type User = {
   id: Generated<string>;
+  createdAt: Generated<Timestamp>;
   email: string;
   userName: string | null;
   gender: Gender | null;
   passwordHash: string;
-  createdAt: Generated<Timestamp>;
   isCompleted: Generated<boolean>;
   authProvider: Generated<AuthProvider>;
 };
 export type DB = {
   accountVerification: AccountVerification;
+  Match: Match;
+  MathConcept: MathConcept;
   refreshTokens: RefreshToken;
+  Ticket: Ticket;
   users: User;
 };
