534022e719e976453d1fa1da8ab9203b45fca80b
kume1a <kumela011@gmail.com>
2023-12-18 15:12:42 +0400

    server time as date

--- src/modules/serverTime/gql/serverTime.object.ts
+++ src/modules/serverTime/gql/serverTime.object.ts
@@ -3,5 +3,5 @@ import { Field, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 export class ServerTimeObject {
   @Field()
-  serverTime: number;
+  serverTime: Date;
 }
--- src/modules/serverTime/serverTime.resolver.ts
+++ src/modules/serverTime/serverTime.resolver.ts
@@ -6,10 +6,8 @@ import { ServerTimeObject } from './gql/serverTime.object';
 export class ServerTimeResolver {
   @Query(() => ServerTimeObject)
   async getServerTime(): Promise<ServerTimeObject> {
-    const payload = new ServerTimeObject();
-
-    payload.serverTime = Date.now();
-
-    return payload;
+    return {
+      serverTime: new Date(),
+    };
   }
 }
