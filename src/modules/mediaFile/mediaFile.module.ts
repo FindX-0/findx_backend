5a82f1adf1c3dbe2eda7f54898a295d98aa558cc
kume1a <kumela011@gmail.com>
2024-01-15 00:49:19 +0400

    apply migration

--- src/modules/mediaFile/mediaFile.module.ts
+++ src/modules/mediaFile/mediaFile.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { MediaFileController } from './mediaFile.controller';
 import { MediaFileRepository } from './mediaFile.repository';
-import { MediaFileQueryService } from './mediaFileQuery.service';
 import { MediaFileValidator } from './mediaFile.validator';
+import { MediaFileQueryService } from './mediaFileQuery.service';
 import { CreateMediaFileUsecase } from './usecase/createMediaFile.usecase';
 import { DeleteMediaFileUsecase } from './usecase/deleteMediaFile.usecase';
 
